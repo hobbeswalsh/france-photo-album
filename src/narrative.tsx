c030104bd3219478c184a5b29c9626a1a07de103
Robin Walsh <rob.walsh@gmail.com>
2026-07-27 15:22:47 -0700

    Adding some more story

--- src/narrative.tsx
+++ src/narrative.tsx
@@ -20,8 +20,8 @@ export const narrative = (
     </p>
     <p>
       After breakfast, we drove our trusty{' '}
-      <Pic name="france-trip-11">steed</Pic> to the dune at Arcachon. After
-      walking up <Pic name="france-trip-12">pavement</Pic> and{' '}
+      <Pic name="france-trip-11">steed</Pic> to France's biggest dune at
+      Arcachon. After walking up <Pic name="france-trip-12">pavement</Pic> and{' '}
       <Pic name="france-trip-13">sand</Pic>, we saw the Atlantic Ocean and got
       some Instagram-ready{' '}
       <Pic
@@ -34,7 +34,44 @@ export const narrative = (
         ]}
       >
         photos.
+      </Pic>{' '}
+      Then we <Pic name="france-trip-19">ran down</Pic> the dune really fast.
+      Our visit to the <Pic name="france-trip-20">"Aquapark"</Pic> is not
+      pictured becuase we didn't bring a phone in. And if anyone can figure out
+      what an "Oxoon Jetkid" is, please get in touch.
+    </p>
+    <p>
+      We finally arrived in Saint-Jean-de-Luz to 40ºC temperatures, so we headed
+      to <Pic name={['france-trip-21', 'france-trip-22']}>the ocean</Pic>. A
+      friend{' '}
+      <Pic
+        name={[
+          'france-trip-23',
+          'france-trip-26',
+          'france-trip-27',
+          'france-trip-28',
+        ]}
+      >
+        (Hi Nicole and Seb!)
+      </Pic>{' '}
+      met us for dinner in fabulous <Pic name="france-trip-25">Guethary</Pic>{' '}
+      and we had a great (if insect-infested) meal.
+    </p>
+    <p>
+      The next morning was Biarritz. It had what you might expect from a coastal
+      French town: <Pic name="france-trip-30">pastries,</Pic>{' '}
+      <Pic name="france-trip-29">surfboards and Acai bowls,</Pic>{' '}
+      <Pic name="france-trip-31">a nice beach,</Pic> and a trail for{' '}
+      <Pic name="france-trip-32">Robin to get lost on</Pic>. There was a cool
+      promenade out to a{' '}
+      <Pic name={['france-trip-33', 'france-trip-35', 'france-trip-39']}>
+        viewpoint
       </Pic>
+      , even if sometimes we{' '}
+      <Pic name={['france-trip-37', 'france-trip-38']}>
+        didn't know what we were viewing
+      </Pic>{' '}
+      exactly.
     </p>
   </>
 )
